872fca73ae3980aef5e30daf7ab7ace31ba8f51d
sunlei <839228534@qq.com>
2019-12-07 23:52:13 +0800

    add database && add post restful

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
 const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
 const mongoose = require('mongoose')
+const Router = require('koa-router')
+const { findData,createData } = require('./server/model/data')
+
 const app = new Koa()
+const router = new Router()
+
+app.use(bodyparser()) //post请求数据获取
 
 mongoose.connect('mongodb://127.0.0.1:27017/note',{
   useNewUrlParser: true,
   useUnifiedTopology: true,
-},() => {
-  console.log(`mongoose connect`)
 })
 
-app.use(bodyparser())
-app.use(async (ctx,next)=> {
-  ctx.body = "hello "
+let db = mongoose.connection
+
+db.once('open', () => console.log("connected to the database"))
+db.on('error', console.error.bind(console, "MongoDB connection error"))
+//数据库连接操作
+
+router.get('/getData', async (ctx,next) => {
+  const data = await findData()
+  ctx.body = {
+    data
+  }
 })
+
+router.post('/pushData', async(ctx,next) => {
+  const {id,message} = ctx.request.body
+  const result = await findData({id})
+  if(result.length !== 0 || !id) {
+    ctx.body = {
+      success: "false",
+      id: result.id,
+      message: `id${result[0].id} 已经存在,无法创建`
+    }
+  }else {
+    const data = await createData(id,message)
+    ctx.body = {
+      success: "true",
+      id: data.id,
+      message: data.message
+    }
+  }
+})
+
+app.use(router.routes(),router.allowedMethods())
+
 
 app.listen(8000,() => {
   console.log('server is running in 8000')  
--- server/model/data/index.js
+++ server/model/data/index.js
@@ -1,5 +1,15 @@
 const Data = require('./data')
 
 module.exports = {
-
+  async findData(query = {}) {
+    const data = await Data.find(query)
+    return data
+  },
+  async createData(id,message) {
+    const data = await Data.create({
+      id,
+      message
+    })
+    return data
+  }
 }
